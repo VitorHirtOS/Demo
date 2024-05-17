a2a599b854c3bf650b83511d07fe6aceffecd3fc
vitor <zorinos.masterzorin@outlook.com>
2024-05-17 13:50:06 -0300

    Update

---
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String layout(){
+        return "home/index";
+    }
+
+
+}
