d849f6b484e0ff2d313aa0525ed6263809ae92c7
Vitor Hirt <zorinos.masterzorin@outlook.com>
2024-05-18 00:59:47 -0300

    BOra

--- demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,16 +1,19 @@
 package com.example.demo.controllers;
 
+import org.apache.tomcat.util.log.UserDataHelper.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String layout(){
+    public String layout(Model model){
+        model.addAttribute("pageTitle", "DedSec");
         return "layout";
     }
 
