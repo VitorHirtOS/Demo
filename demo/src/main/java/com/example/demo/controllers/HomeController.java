698b451cc0b84332e2b149703602754076691c64
Vitor Hirt <zorinos.masterzorin@outlook.com>
2024-05-18 16:13:24 -0300

    update

--- demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,11 +1,7 @@
 package com.example.demo.controllers;
 
-import org.apache.tomcat.util.log.UserDataHelper.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.Model;
 
 @Controller
